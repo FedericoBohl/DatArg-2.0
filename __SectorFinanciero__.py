74292835e3f5ff975ba6986322d1fe82b895f17d
FedericoBohl <federicobohl@uca.edu.ar>
2024-05-22 23:10:05 -0300

    test

--- __SectorFinanciero__.py
+++ __SectorFinanciero__.py
@@ -105,6 +105,9 @@ def make_merv():
         st.write(df_bonos_cor)
         st.header("IAMC??")
         st.write(df_iamc)
+        st.divider()
+        st.subheader("ejemplo de filtrado de bonos")
+        st.write(df_iamc['Especie'].to_list())
     with acciones: 
         fig_merv,fig_gen=make_acciones(df_merval,df_general)
         container=st.container(border=True)
