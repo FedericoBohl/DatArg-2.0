7e90a0ed3870af42b988a6e86616647e14b12415
FedericoBohl <federicobohl@uca.edu.ar>
2024-05-29 20:51:06 -0300

    test

--- __SectorInternacional__.py
+++ __SectorInternacional__.py
@@ -3,10 +3,11 @@ from librerias import *
 @st.cache_resource(show_spinner=False)
 def get_eu():
     mro=pd.read_csv('https://data-api.ecb.europa.eu/service/data/FM/D.U2.EUR.4F.KR.MRR_FR.LEV?startPeriod=2000-01&detail=dataonly&format=csvdata')
+    st.write(mro)
     mro=mro[['TIME_PERIOD','OBS_VALUE']]
     mro.TIME_PERIOD=pd.to_datetime(mro.TIME_PERIOD, format='%Y-%m-%d')
     mro=mro.set_index('TIME_PERIOD',inplace=True)
-    mro=mro.resample('M').last()
+    mro.resample('M').last()
     mro=mro.rename(columns={'OBS_VALUE':'MRO'})
     mro.index=mro.index.strftime('%b-%Y')
 
