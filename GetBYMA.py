69808b3f308a660e670ece1d14d112ffca75aa10
FedericoBohl <federicobohl@uca.edu.ar>
2024-05-28 22:47:24 -0300

    test

--- GetBYMA.py
+++ GetBYMA.py
@@ -136,7 +136,7 @@ def GetBYMA():
                 df.columns = __fixedIncome_columns
                 df.set_index('Nombre', inplace=True)
                 df[~df.index.duplicated(keep='first')]
-                df_bonos_cor= df[['Precio','Var%','Moneda','Fecha de madurez', 'Moneda', 'Días hasta la maturity', 'Volumen','Cantidad Ofrecida']]
+                df_bonos_cor= df[['Precio','Var%','Moneda','Fecha de madurez', 'Días hasta la maturity', 'Volumen','Cantidad Ofrecida']]
             except: df_bonos_cor=None
 
             try: 
