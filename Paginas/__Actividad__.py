ae8a0c14633ecf8c05d00bb484bd807beae9a077
FedericoBohl <federicobohl@uca.edu.ar>
2024-07-09 19:57:36 -0300

    test

--- Paginas/__Actividad__.py
+++ Paginas/__Actividad__.py
@@ -3,13 +3,13 @@ from librerias import *
 
 def make_actividad_web():
     c1,c2=st.columns(2)
-    with c1.container(border=True):
+    with c1.container(border=False):
         st.header('PBI Real')
-    with c2.container(border=True):
+    with c2.container(border=False):
         st.header('Componentes sobre PBI')
     c1,c2=st.columns(2)
-    with c1.container(border=True):
+    with c1.container(border=False):
         st.header('Actividad')
         st.caption('''* Emae general\n* Emae campo\n* Emae construcción\n* Etc.''')
-    with c2.container(border=True):
+    with c2.container(border=False):
         st.header('PBI en USD y PBI per cápita')
