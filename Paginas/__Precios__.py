b47ac6b6d411356f2f2f19de3d1a23c034f9b5cc
FedericoBohl <federicobohl@uca.edu.ar>
2024-07-09 19:56:36 -0300

    test

--- Paginas/__Precios__.py
+++ Paginas/__Precios__.py
@@ -3,12 +3,12 @@ from librerias import *
 
 def make_precios_web():
     c1,c2=st.columns(2)
-    with c1.container(border=True):
+    with c1.container(border=False):
         st.header('Inflación')
-    with c2.container(border=True):
+    with c2.container(border=False):
         st.header('Componentes del IPC')
     c1,c2=st.columns(2)
-    with c1.container(border=True):
+    with c1.container(border=False):
         st.header('TC y Brecha')
-    with c2.container(border=True):
+    with c2.container(border=False):
         st.header('Expectativas de Inflación')
