866427ff01cda0058aa4b912f563aae7bc99fb17
FedericoBohl <federicobohl@uca.edu.ar>
2024-07-09 20:24:24 -0300

    test

--- Paginas/__Precios__.py
+++ Paginas/__Precios__.py
@@ -1,10 +1,19 @@
 from librerias import *
+from streamlit_extras.stylable_container import stylable_container as cont
 
 
 def make_precios_web():
     c1,c2=st.columns(2)
-    with c1.container(border=False):
-        st.header('Inflación')
+    with c1:
+        with cont(css_styles="""
+                            {
+                            background-color:white;
+                            padding:5px;
+                            border-radius:15px;        
+                            box-shadow: 0px 0px 10px rgba(255, 255, 255, 0.5)  
+                            }    
+                            """):
+            st.header('Inflación')
     with c2.container(border=False):
         st.header('Componentes del IPC')
     c1,c2=st.columns(2)
