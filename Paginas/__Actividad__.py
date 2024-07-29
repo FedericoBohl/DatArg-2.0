910541ef9b6f63a4bb09d19b31af55f2727a42d5
FedericoBohl <federicobohl@uca.edu.ar>
2024-07-29 16:29:25 -0300

    detalle

--- Paginas/__Actividad__.py
+++ Paginas/__Actividad__.py
@@ -206,22 +206,22 @@ def make_actividad_web():
     pbi.index=pbi.index.strftime('%b-%Y')
 
     c1,c2,c3=st.columns(3)
-    with c1.container(border=False):
+    with c1.container(border=True):
         st.subheader('EMAE')
         st.radio('EMAE-elegido',label_visibility='collapsed',options=['EMAE-Nivel General','Agricultura, ganadería, caza y silvicultura','Explotación de minas y canteras','Comercio mayorista, minorista y reparaciones','Actividades inmobiliarias, empresariales y de alquiler'],key='emae_elegido')
         emae_plots=plot_emae(actividad)
         st.plotly_chart(emae_plots[S.emae_elegido],config={'displayModeBar': False},use_container_width=True)
-    with c2.container(border=False):
+    with c2.container(border=True):
         st.subheader('Industria')
         plot_ipi(actividad)
     with c3.container(border=True):
         st.subheader('Construcción')
         plot_isac(actividad)
     c1,c2=st.columns(2)
-    with c1.container(border=False):
+    with c1.container(border=True):
         st.subheader('PBI Real')
         plot_PBI(pbi)
-    with c2.container(border=False):
+    with c2.container(border=True):
         st.subheader('PBI per cápita')   
         plot_percap(pbi)
     st.divider()
