58c58613f32a7c4dbe06c4e93ebf58526e15013b
FedericoBohl <federicobohl@uca.edu.ar>
2024-07-30 17:47:29 -0300

    test

--- Calendar/calendar.py
+++ Calendar/calendar.py
@@ -3,7 +3,7 @@ import pandas as pd
 from streamlit_calendar import calendar
 
 
-@st.cache_resource(show_spinner=False)
+@st.cache_data(show_spinner=False)
 def create_calendar(df):
     # Convertir columna de fechas a tipo datetime
     df['date'] = pd.to_datetime(df['date'])
