413bfc443f74f7aa4e1456a1bd7cc4c4fccf8fc2
FedericoBohl <federicobohl@uca.edu.ar>
2024-07-31 19:53:27 -0300

    Estructura: ahora cuando carga los datos se dirige a la página "login_animation.py" y luego, o en caso de que ya esten cargados, se dirige y queda en la página "app.py"

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-from _globals_ import *
 import streamlit as st
 from streamlit import session_state as S
-import pandas as pd
-from librerias import load_lottieurl,get_pbi
-import streamlit.components.v1 as components
-from datetime import datetime
-from streamlit_lottie import st_lottie_spinner
-from Paginas.__BCRA__ import make_BCRA_web,load_bcra
-from Paginas.__SectorExterno__ import make_sect_ext_web,load_sect_ext
-from Paginas.__SectorPublico__ import make_sect_pub_web,load_data_map,load_data_sectpub,load_datos_deuda
-from Paginas.__SectorInternacional__ import make_internacional_web
-from Paginas.__SectorFinanciero__ import make_merv_web
-from Paginas.__Actividad__ import make_actividad_web,load_actividad
-from Paginas.__Pobreza__ import make_pobreza_web
-from Paginas.__Precios__ import make_precios_web
-from Calendar.calendar import create_calendar
-
 
 st.set_page_config(
     page_title="DatArg",
@@ -23,81 +7,8 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded")
 
-#@st.cache_resource(show_spinner=False)
-def load_ALL(today):
-    S.pbi_men=get_pbi()
-    S.actividad_,S.pbi_=load_actividad(today)
-    S.reservas_,S.bcra_,S.bcragdp_,S.datatco_,S.tasas_,S.TCR,S.TC=load_bcra(today)
-    S.bop_,S.bopgdp_,S.ica_,S.icagdp_,S.tot=load_sect_ext(today)
-    S.deficit_,S.datagdp_,S.datatco_,S.endeudamiento_,S.endeudamientogdp_,S.endeudamientotco_,S.corr_,S.corrgdp_,S.corrtco_=load_data_sectpub(today)
-    S.deuda,S.deuda_mon=load_datos_deuda(today)
-    S.data_map,S.geo_map,S.extras_map=load_data_map(today)
-
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-local_css('styles.css')
-#Estilado de la página
-st.markdown('''<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>''', unsafe_allow_html=True)
-#st.markdown("<link href='https://fonts.googleapis.com/css?family=EB Garamond' rel='stylesheet'>",unsafe_allow_html=True)
-#st.markdown("<style>body {font-family: 'EB Garamond';font-size: 22px;}</style>",unsafe_allow_html=True)
-
-
-#@st.cache_resource(show_spinner=False)
-today=datetime.now().strftime("%Y%m%d")
 if not '__loaded__' in S:
-    cont=st.container(border=False,height=500)
-    lottie_progress_url = "https://lottie.host/61385cf3-564b-41cb-a243-3ce5c25c4134/uIUPGURgQ9.json"
-    lottie_progress = load_lottieurl(lottie_progress_url)
-    with st_lottie_spinner(lottie_progress, loop=True, key="progress",height=100):
-        load_ALL(today)
-    S.__loaded__=0
-#else:
-#    load_ALL(today)
+    st.switch_page('login_animation.py')
 
+st.switch_page('app.py')
 
-#***************************************    ARMADO DE LA PÁGINA     ****************************************************************
-components.html(w_barra_stocks,height=80)
-col1,col2=st.columns((0.1,0.9))
-with col1:st.image("Icono.jpeg",caption="🐐")
-with col2:
-
-    #x=f"""<div data-stale="false" width="{page_width}"""+"""class="element-container st-emotion-cache-u4g42f e1f1d6gn4" data-testid="element-container"><div class="stHeadingContainer" data-testid="stHeading"><div class="stMarkdown" style="width: 524.8px;"><div data-testid="stMarkdownContainer" class="st-emotion-cache-w3enc8 e1nzilvr5" style="width: 524.8px;"><div class="st-emotion-cache-1629p8f e1nzilvr2"><h2 id="61901152"><div data-testid="StyledLinkIconContainer" class="st-emotion-cache-zt5igj e1nzilvr4"><a href="#61901152" class="st-emotion-cache-p2doo6 e1nzilvr3"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg></a><span class="st-emotion-cache-10trblm e1nzilvr1"><span style='text-align: center; color: #6CACE4; font-family: "Source Serif Pro", serif; font-weight: 600; letter-spacing: -0.005em; padding: 1rem 0px; margin: 0px; line-height: 1.2;'>Datos Argentina 🧉</span></span></div></h2></div></div></div><hr data-testid="stHeadingDivider" color="#FFB81C" class="st-emotion-cache-h8phe6 e1nzilvr0"></div></div>"""
-    #st.markdown(x,unsafe_allow_html=True)
-    #st.markdown("""<h2 style='text-align: center; color: #6CACE4; font-family: "Source Serif Pro", serif; font-weight: 600; letter-spacing: -0.005em; padding: 1rem 0px; margin: 0px; line-height: 1.2;'>Datos Argentina 🧉</h2>""", unsafe_allow_html=True)
-    #st.markdown("""<hr data-testid="stHeadingDivider" color="#3d9df3" class="st-emotion-cache-h8phe6 e1nzilvr0">""",unsafe_allow_html=True)
-    #st.markdown("""<hr data-testid="stHeadingDivider" color="#6cace4" class="st-emotion-cache-h8phe6 e1nzilvr0">""", unsafe_allow_html=True)
-    st.header('Datos Argentina 🧉',divider='blue')
-    c1,c2=st.columns((0.8,0.2))
-    with c1.popover("Calendario Económico",use_container_width=True,help="Los datos muy recientes pueden tardar unos pocos dias en ser agregados a las series oficiales. Aquí puede ver los últimos datos anunciados y las fechas de proximos anuncios."):
-    #with st.expander(label='Calendario económico',icon=":material/settings:"):
-        #create_widget(w_calendar_tv,height=350,width=int(S.page_width*0.85))
-        df = pd.read_csv('Calendar/calendar_events.csv')
-        calendario=create_calendar(df)
-    with c2.popover('Bot de Telegram/Mail',use_container_width=True):
-        st.caption('Poner algo corto para explicar de que trata el bot y el link a este o un formulario de inscripción (chequear esto de abajo)')
-        st.write(st.form_submit_button)
-t_info, t_actividad, t_PI, t_precios, t_bcra, t_SecExt, t_SecPub, t_Intl, t_Merv= st.tabs(["Info","Actividad","Pobreza y Empleo", "Precios", "BCRA", "Sector Externo","Sector Público","Internacional","Bolsa Argentina"])
-
-
-
-with t_actividad:
-    make_actividad_web()
-with t_PI:
-    make_pobreza_web()
-with t_precios:
-    make_precios_web()
-with t_bcra:
-    make_BCRA_web()
-with t_SecExt:
-    make_sect_ext_web()
-with t_SecPub:
-    make_sect_pub_web()
-with t_Intl:
-    make_internacional_web()
-with t_Merv:
-    make_merv_web()
-
-
-
-#st.write(st.context.headers)
---
+++ pages/login_animation.py
@@ -0,0 +1,34 @@
+from Paginas.__BCRA__ import load_bcra
+from Paginas.__SectorExterno__ import load_sect_ext
+from Paginas.__SectorPublico__ import load_data_map,load_data_sectpub,load_datos_deuda
+from Paginas.__Actividad__ import load_actividad
+from librerias import load_lottieurl,get_pbi
+from streamlit_lottie import st_lottie_spinner # type: ignore
+import streamlit as st
+from streamlit import session_state as S
+from datetime import datetime
+
+st.set_page_config(
+    page_title="DatArg",
+    page_icon="🧉",
+    layout="centered",
+    initial_sidebar_state="collapsed")
+def load_ALL(today):
+    S.pbi_men=get_pbi()
+    S.actividad_,S.pbi_=load_actividad(today)
+    S.reservas_,S.bcra_,S.bcragdp_,S.datatco_,S.tasas_,S.TCR,S.TC=load_bcra(today)
+    S.bop_,S.bopgdp_,S.ica_,S.icagdp_,S.tot=load_sect_ext(today)
+    S.deficit_,S.datagdp_,S.datatco_,S.endeudamiento_,S.endeudamientogdp_,S.endeudamientotco_,S.corr_,S.corrgdp_,S.corrtco_=load_data_sectpub(today)
+    S.deuda,S.deuda_mon=load_datos_deuda(today)
+    S.data_map,S.geo_map,S.extras_map=load_data_map(today)
+
+today=datetime.now().strftime("%Y%m%d")
+
+cont=st.container(border=False,height=500)
+with cont:
+    lottie_progress_url = "https://lottie.host/61385cf3-564b-41cb-a243-3ce5c25c4134/uIUPGURgQ9.json"
+    lottie_progress = load_lottieurl(lottie_progress_url)
+    with st_lottie_spinner(lottie_progress, loop=True, key="progress",height=490):
+        load_ALL(today)
+S.__loaded__=0
+st.switch_page('app.py')
