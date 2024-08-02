6ccfb2c5fa0c7ee1fac27b4217f7990962468f18
FedericoBohl <federicobohl@uca.edu.ar>
2024-08-02 19:20:12 -0300

    fix

--- Paginas/__SectorInternacional__.py
+++ Paginas/__SectorInternacional__.py
@@ -20,7 +20,7 @@ def load_canasta(end):
         'COP':[0.0604214503862096,'https://www.google.com/finance/quote/USD-COP?hl=es'],
         'BRL':[0.301541171878207,'https://www.google.com/finance/quote/USD-BRL?hl=es'],
         'MXN':[0.484520491738942,'https://www.google.com/finance/quote/USD-MXN?hl=es'],
-        'CPL':[0.0842257220859683,'https://www.google.com/finance/quote/USD-CLP?hl=es']
+        'CLP':[0.0842257220859683,'https://www.google.com/finance/quote/USD-CLP?hl=es']
         }
     total=0
     for i in data:
@@ -343,12 +343,13 @@ def make_internacional_web():
                             )
                         )
                     )  
+            fig['layout']['yaxis2']['type']='log'
             st.plotly_chart(fig,config={'displayModeBar': False},use_container_width=True)
         with c2:
             st.caption('Variaciónes Intermensuales !!')
             c21,c22=st.columns(2)
-            c21.metric('Argentina',fx['ARS'][1],delta=f'Share: {(100*fx["ARS"][0]):.2f}%',delta_color='off')
-            c22.metric('Brasil',fx['BRL'][1],delta=f'Share: {(100*fx["BRL"][0]):.2f}%',delta_color='off')
-            c21.metric('Colombia',fx['COP'][1],delta=f'Share: {(100*fx["COP"][0]):.2f}%',delta_color='off')
-            c22.metric('Chile',fx['CLP'][1],delta=f'Share: {(100*fx["CLP"][0]):.2f}%',delta_color='off')
-            c21.metric('México',fx['MXN'][1],delta=f'Share: {(100*fx["MXN"][0]):.2f}%',delta_color='off')
+            c21.metric('Argentina',f"{fx['ARS'][1]:.2f} USD",delta=f'Share: {(100*fx["ARS"][0]):.2f}%',delta_color='off')
+            c22.metric('Brasil',f"{fx['BRL'][1]:.2f} USD",delta=f'Share: {(100*fx["BRL"][0]):.2f}%',delta_color='off')
+            c21.metric('Colombia',f"{fx['COP'][1]:.2f} USD",delta=f'Share: {(100*fx["COP"][0]):.2f}%',delta_color='off')
+            c22.metric('Chile',f"{fx['CLP'][1]:.2f} USD",delta=f'Share: {(100*fx["CLP"][0]):.2f}%',delta_color='off')
+            c21.metric('México',f"{fx['MXN'][1]:.2f} USD",delta=f'Share: {(100*fx["MXN"][0]):.2f}%',delta_color='off')
