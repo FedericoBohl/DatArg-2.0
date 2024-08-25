89bfbd7e0ca7ad071002ae36e3a3bda21057e1ac
FedericoBohl <federicobohl@uca.edu.ar>
2024-08-25 16:57:12 -0300

    test

--- Paginas/__Precios__.py
+++ Paginas/__Precios__.py
@@ -67,11 +67,11 @@ def plot_inflacion(data,rem,start,end):
     #    fig.add_trace(go.Bar(x=rem.index.strftime('%b-%Y')[-1],y=rem['REM'][-1]*100,name='Infl. Esperada-IA',marker_color='crimson',showlegend=False,legendgroup='rem'),secondary_y=True)
     if (start in range (2007,2015)) or (end in range (2007,2015)) or (2007<=end and 2015>=start):
         fig.add_vrect(x0=f"{max(2007,start)}-01", x1=f"{min(2015,end)}-12", 
-            fillcolor="lightslategrey", line_width=0,label=dict(text="Intervención del INDEC",textposition="top center",font=dict(size=14, color='black')))
+            fillcolor="lightslategrey", opacity=0.25, line_width=0,label=dict(text="Intervención del INDEC",textposition="top center",font=dict(size=14, color='black')))
     fig.update_layout(margin=dict(l=1, r=1, t=75, b=1),height=450,bargap=0.2,legend=dict(
                                         orientation="h",
                                         yanchor="bottom",
-                                        y=-0.3,
+                                        y=-0.75,
                                         xanchor="center",
                                         x=0.5,
                                         bordercolor=black,
