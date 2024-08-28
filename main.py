0608fcdaecd3ef97f3f8bb0e8ad1dc50621f9173
FedericoBohl <federicobohl@uca.edu.ar>
2024-08-28 19:54:27 -0300

    test

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def load_lottieurl(url: str):
     return r.json()
 
 def make_info():
-    cafecito='''<a href='https://cafecito.app/datarg' rel='noopener' target='_blank'><img srcset='https://cdn.cafecito.app/imgs/buttons/button_6.png 1x, https://cdn.cafecito.app/imgs/buttons/button_6_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_6_3.75x.png 3.75x' src='https://cdn.cafecito.app/imgs/buttons/button_6.png' alt='Invitame un café en cafecito.app' /></a>'''
+    cafecito='''<style>justify-content: center</style><a href='https://cafecito.app/datarg' rel='noopener' target='_blank'><img srcset='https://cdn.cafecito.app/imgs/buttons/button_6.png 1x, https://cdn.cafecito.app/imgs/buttons/button_6_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_6_3.75x.png 3.75x' src='https://cdn.cafecito.app/imgs/buttons/button_6.png' alt='Invitame un café en cafecito.app' /></a>'''
     st.html(cafecito)
 
 
