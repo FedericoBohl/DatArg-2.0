8aea63478483db9357b115c61bc453d4d1d65357
FedericoBohl <federicobohl@uca.edu.ar>
2024-09-01 19:56:41 -0300

    test

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ st.set_page_config(
 
 #***************************************    ARMADO DE LA PÁGINA     ****************************************************************
 
-@st.cache_resource(show_spinner=False)
+#@st.cache_resource(show_spinner=False)
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
