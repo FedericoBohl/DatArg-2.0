1decf4a3023b656ccde5ae5117e9db2f07dc9208
FedericoBohl <federicobohl@uca.edu.ar>
2024-09-03 16:24:02 -0300

    test

--- Paginas/__SectorPublico__.py
+++ Paginas/__SectorPublico__.py
@@ -414,7 +414,7 @@ def make_map(data,geo,extras:pd.DataFrame,eleccion):
     # Actualizar el layout de la figura
     fig.update_layout(
         mapbox=dict(
-            center={"lat": -38.4161, "lon": -63.6167},
+            center={"lat": -30.4161, "lon": -63.6167},
             style="white-bg",
             zoom=2.65,
             layers=[dict(below='traces', type='fill', source=geo, color="lightblue")]
