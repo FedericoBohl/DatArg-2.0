d4bb789f16a0862e0d176cf9b765077ad17954e8
FedericoBohl <federicobohl@uca.edu.ar>
2024-09-04 22:57:20 -0300

    test

--- main.py
+++ main.py
@@ -172,3 +172,11 @@ with st.container():
         st.toast('Muchas gracias por tu comentiario y por ayudarnos a mejorar la página!')
         S.suggestion=None
         st.rerun()
+        
+form_url = 'https://docs.google.com/forms/d/11ePhY_ruI2j3-QHg5xfyL1UXxMI5GkaFc7w1kn7ImRs/edit'
+
+# Mostrar el enlace al formulario
+st.markdown(f"[Abrir formulario de Google](<{form_url}>)")
+
+# También podrías incluir el formulario directamente en tu aplicación usando un iframe
+st.markdown(f'<iframe src="{form_url}" width="100%" height="600px"></iframe>', unsafe_allow_html=True)
