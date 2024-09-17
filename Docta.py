d6cb8a50586c62b1bba37b5d455728cb2dad1d12
FedericoBohl <federicobohl@uca.edu.ar>
2024-09-17 18:49:31 -0300

    test

--- Docta.py
+++ Docta.py
@@ -3,17 +3,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 import pandas as pd
 import time
+import streamlit as st
 
+@st.cache_resource
+def get_driver(options):
+    return webdriver.Chrome(
+        service=Service(
+            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+        ),
+        options=options,
+    )
 class DoctaCap:
     def __init__(self):
         chrome_options = Options()
         chrome_options.add_argument("--headless")  # Ejecutar en modo headless
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        self.driver = webdriver.Chrome(options=chrome_options)
+        chrome_options.add_argument("--disable-gpu")
+        self.driver = get_driver(chrome_options)
         self.token_info = None
         self.urls={'CER':'https://www.doctacapital.com.ar/dashboard/bonos/general/cer',
                    'DL':'https://www.doctacapital.com.ar/dashboard/bonos/general/dollar-linked',
