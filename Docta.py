02183d7f6f43f6892307fd0af0bafa16b7299727
FedericoBohl <federicobohl@uca.edu.ar>
2024-09-17 18:24:17 -0300

    test-DoctaCap

---
+++ Docta.py
@@ -0,0 +1,91 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+import time
+
+class DoctaCap:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.token_info = None
+        self.urls={'CER':'https://www.doctacapital.com.ar/dashboard/bonos/general/cer',
+                   'DL':'https://www.doctacapital.com.ar/dashboard/bonos/general/dollar-linked',
+                   'Soberanos':'https://www.doctacapital.com.ar/dashboard/bonos/general/hard-dollar',
+                   'ON':'https://www.doctacapital.com.ar/dashboard/bonos/general/on',
+                   'BOP':'https://www.doctacapital.com.ar/dashboard/bonos/general/bopreal',
+                   'Acciones':'https://www.doctacapital.com.ar/dashboard/acciones/general',
+                   'Cedears':'https://www.doctacapital.com.ar/dashboard/cedears/general',
+                   'lecaps':'https://www.doctacapital.com.ar/dashboard/bonos/general/fixed-rate'
+                    }
+        self.df={'CER':None,
+                   'DL':None,
+                   'Soberanos':None,
+                   'ON':None,
+                   'BOP':None,
+                   'Acciones':None,
+                   'Cedears':None,
+                   'lecaps':None
+                    }
+        for key in self.df:
+            try:
+                self.df[key]=self.extract_table(self.urls[key])
+            except:continue
+        try:
+            self.df['Soberanos']=self.df['Soberanos'][~self.df['Soberanos'].index.str.endswith('C')]
+            self.df['Soberanos']=pd.concat([self.df['Soberanos'],self.df['BOP']])
+        except:pass
+    def extract_table(self,url)->pd.DataFrame:
+        data=[['Sin resultados.']]
+        while data[0][0]=='Sin resultados.':
+            try:
+                self.driver.get(url=url)
+                wait = WebDriverWait(self.driver, 5)
+                button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[normalize-space(text())='Limpiar filtros']")))
+                button.click()
+                time.sleep(2.5)
+
+                wait = WebDriverWait(self.driver, 5)  # Esperar hasta 30 segundos
+                wait.until(EC.presence_of_element_located((By.TAG_NAME, 'table')))
+
+                # Encuentra la última ngx-datatable
+                element=self.driver.find_element(By.TAG_NAME, 'table')
+                head=element.find_element(By.TAG_NAME,'thead')
+                body=element.find_element(By.TAG_NAME,'tbody')
+                cols= [i.text for i in head.find_elements(By.TAG_NAME,'th')]
+                data=[[td.text for td in tr.find_elements(By.TAG_NAME,'td')] for tr in body.find_elements(By.TAG_NAME,'tr')]
+            except Exception as e:
+                print(e,'\n')
+                continue
+        data=pd.DataFrame(data,columns=cols)
+        data=data[data['Ticker']!='tc-mayorista']
+        tickers=[]
+        tipo=[]
+        for row in data['Ticker']:
+            tickers.append(row.split('\n')[0])
+            tipo.append(row.split('\n')[1])
+        data['Ticker']=tickers
+        data['Tipo']=tipo
+        data=data[data['Tipo']=='24hs']
+        data['Último precio']=[float(i.split(' ')[0].replace('$\n','').replace('.','').replace(',','.')) for i in data['Último precio']]
+        data['Variación']=[float(i.replace('+','').replace('\n%','').replace(',','.')) for i in data['Variación']]
+
+        if 'TIR' in cols:
+            data['TIR']=[float(i.replace('.','').replace('%','').replace(',','.')) for i in data['TIR']]
+            data['MD']=[float(i.replace('.','').replace(',','.')) for i in data['MD']]
+            data['Valor técnico']=[float(i.replace('.','').replace(',','.')) for i in data['Valor técnico']]
+            data['Paridad']=[float(i.replace('.','').replace('%','').replace(',','.')) for i in data['Paridad']]
+            data=data[['Ticker', 'Último precio', 'Variación','TIR', 'MD','Valor técnico', 'Paridad']]
+        elif 'TEM' in cols:
+            data['TEM']=[float(i.replace('.','').replace('%','').replace(',','.')) for i in data['TEM']]
+            data['MD']=[float(i.replace(',','.')) for i in data['MD']]
+            data['Valor técnico']=[float(i.replace('.','').replace(',','.')) for i in data['Valor técnico']]
+            data['Paridad']=[float(i.replace('.','').replace('%','').replace(',','.')) for i in data['Paridad']]
+            data=data[['Ticker', 'Último precio', 'Variación','TEM', 'MD','Valor técnico', 'Paridad']]
+        else:
+            data['Último precio']=[float(i.split(' ')[0].replace('$\n','').replace('.','').replace(',','.')) for i in data['Último precio']]
+            data['Variación']=[float(i.replace('+','').replace('\n%','').replace(',','.')) for i in data['Variación']]
+            data=data[['Ticker', 'Último precio', 'Variación']]
+        data.set_index('Ticker',inplace=True)
+        return data
--- Paginas/__SectorFinanciero__.py
+++ Paginas/__SectorFinanciero__.py
@@ -1,4 +1,5 @@
-from GetBYMA import GetBYMA
+#from GetBYMA import GetBYMA
+from Docta import DoctaCap
 from _globals_ import *
 import streamlit as st
 from streamlit import session_state as S
@@ -101,77 +102,78 @@ def make_acciones(data_now_merv : pd.DataFrame , data_now_gen : pd.DataFrame):
     #fig_gen.update_layout(margin=dict(l=1, r=1, t=10, b=1))
     return fig_merv,None#,fig_gen
 
+def make_bonds():
+    c1_1,c2_1,c3_1=st.columns(3)
+    #Los dataframes deberían tener de index el ticker del bono para hacer el filtrado más simple
+    with c1_1:
+        if isinstance(S.docta.df['Soberanos'],pd.DataFrame):
+            st.subheader('Títulos Públicos')
+            t_1_nac,t_2_nac=st.tabs(['Panel','Curva'])
+            with t_1_nac: st.subheader('Panel');st.dataframe(S.docta.df['Soberanos'])
+            with t_2_nac: st.subheader('Curva')
+        else: st.exception(Exception('Error en la carga de datos desde ByMA. Disculpe las molestias, estamos trabajando para solucionarlo.'))
+    with c2_1:
+        if S.df_bonos_gob is not None:
+            st.subheader('Bonos Dollar Linked')
+            t_1_ex,t_2_ex,t_3_ex=st.tabs(['Panel','Curva','Buscador'])
+            with t_1_ex: st.subheader('Panel')
+            with t_2_ex: st.subheader('Curva')
+            with t_3_ex: st.dataframe(S.df_bonos_gob[['last','change','volume','expiration']])#Idem
+        else: st.exception(Exception('Error en la carga de datos desde ByMA. Disculpe las molestias, estamos trabajando para solucionarlo.'))
+    with c3_1:
+        if S.df_bonos_gob is not None:
+            st.subheader('Bonos ajustados por CER')
+            t_1_c,t_2_c,t_3_c=st.tabs(['Panel','Curva','Buscador'])
+            with t_1_c: st.subheader('Panel')
+            with t_2_c: st.subheader('Curva')
+            with t_3_c: st.dataframe(S.df_bonos_gob[['last','change','volume','expiration']])#Idem
+        else: st.exception(Exception('Error en la carga de datos desde ByMA. Disculpe las molestias, estamos trabajando para solucionarlo.'))
+    c1_2,c2_2=st.columns(2)
+    with c1_2:
+        if S.df_letras is not None:
+            st.subheader('Lecaps')
+            t_1_l,t_2_l,t_3_l=st.tabs(['Panel','Curva','Buscador'])
+            with t_1_l: st.subheader('Panel')
+            with t_2_l: st.subheader('Curva')
+            with t_3_l: st.dataframe(S.df_letras[['last','change','volume','expiration']])
+        else: st.exception(Exception('Error en la carga de datos desde ByMA. Disculpe las molestias, estamos trabajando para solucionarlo.'))
+    with c2_2:    
+        if S.df_bonos_cor.all() is not None:
+            st.subheader('Obligaciones Negociables')
+            t_1_cor,t_2_cor,t_3_cor=st.tabs(['Panel','Curva','Buscador'])
+            with t_1_cor: st.dataframe(S.df_bonos_cor,use_container_width=True)
+            with t_2_cor: st.subheader('Curva')
+            with t_3_cor: 
+                st.selectbox('Buscador de corpo',label_visibility='collapsed',options=S.df_bonos_cor.index.to_list(),key='corpobuscado')
+                st.dataframe(S.df_bonos_cor.loc[S.df_bonos_cor.index==S.corpobuscado].transpose(),use_container_width=True)
+        else: st.exception(Exception('Error en la carga de datos desde ByMA. Disculpe las molestias, estamos trabajando para solucionarlo.'))
+    if S.df_iamc is not None:
+        c1_3,c2_3=st.columns((0.7,0.3))
+        with c1_3:
+            st.subheader('Información de los bonos')
+            st.dataframe(S.df_iamc)
+        with c2_3:
+            st.subheader('Filtado de bono')
+            st.selectbox('Buscador de Bonos',options=S.df_iamc.index.to_list(),key='bonobuscado')
+            st.dataframe(S.df_iamc.loc[S.df_iamc.index==S.bonobuscado].transpose(),use_container_width=True)
+    else: st.exception(Exception('Error en la carga de datos desde ByMA. Disculpe las molestias, estamos trabajando para solucionarlo.'))
+    st.divider()
+    #TEST
+    try:
+        _=pd.merge(S.df_bonos_gob[['last','change','volume','expiration']],S.df_iamc[['Duración Modificada','Tir Anual']], left_index=True,right_index=True)
+        st.dataframe(_)
+    except:pass
+
+
 def make_merv_web():
     st.header('Mercado de Capitales')
     st.button('🔄️Recargar Datos',key='Reload')
     try:
-        if (not 'df_indice' in S) or S.Reload:
-            S.df_indice,S.df_bonos_gob,S.df_letras,S.df_bonos_cor,S.df_merval,S.df_general,S.df_cedears, S.df_iamc=GetBYMA()
-
-        st.metric('Merval',S.df_indice.loc[1,'last'],f'{round(S.df_indice.loc[1,"change"]*100,2)}%')
-        bonos, acciones, cedears= st.tabs(["Bonos", "Acciones",'Cedears'])
+        if (not 'docta' in S):
+            S.docta=DoctaCap()
+        bonos, acciones, cedears, forex= st.tabs(["Bonos", "Acciones",'Cedears','Forex'])
         with bonos:
-            c1_1,c2_1,c3_1=st.columns(3)
-            #Los dataframes deberían tener de index el ticker del bono para hacer el filtrado más simple
-            with c1_1:
-                if S.df_bonos_gob is not None:
-                    st.subheader('Bonos Soberanos')
-                    t_1_nac,t_2_nac,t_3_nac=st.tabs(['Panel','Curva','Buscador'])
-                    with t_1_nac: st.subheader('Panel')
-                    with t_2_nac: st.subheader('Curva')
-                    with t_3_nac: st.dataframe(S.df_bonos_gob[['last','change','volume','expiration']])#Filtrar por rango
-                else: st.exception(Exception('Error en la carga de datos desde ByMA. Disculpe las molestias, estamos trabajando para solucionarlo.'))
-            with c2_1:
-                if S.df_bonos_gob is not None:
-                    st.subheader('Bonos Ley Extrangera')
-                    t_1_ex,t_2_ex,t_3_ex=st.tabs(['Panel','Curva','Buscador'])
-                    with t_1_ex: st.subheader('Panel')
-                    with t_2_ex: st.subheader('Curva')
-                    with t_3_ex: st.dataframe(S.df_bonos_gob[['last','change','volume','expiration']])#Idem
-                else: st.exception(Exception('Error en la carga de datos desde ByMA. Disculpe las molestias, estamos trabajando para solucionarlo.'))
-            with c3_1:
-                if S.df_bonos_gob is not None:
-                    st.subheader('Bonos ajustados por CER')
-                    t_1_c,t_2_c,t_3_c=st.tabs(['Panel','Curva','Buscador'])
-                    with t_1_c: st.subheader('Panel')
-                    with t_2_c: st.subheader('Curva')
-                    with t_3_c: st.dataframe(S.df_bonos_gob[['last','change','volume','expiration']])#Idem
-                else: st.exception(Exception('Error en la carga de datos desde ByMA. Disculpe las molestias, estamos trabajando para solucionarlo.'))
-            c1_2,c2_2=st.columns(2)
-            with c1_2:
-                if S.df_letras is not None:
-                    st.subheader('Letras')
-                    t_1_l,t_2_l,t_3_l=st.tabs(['Panel','Curva','Buscador'])
-                    with t_1_l: st.subheader('Panel')
-                    with t_2_l: st.subheader('Curva')
-                    with t_3_l: st.dataframe(S.df_letras[['last','change','volume','expiration']])
-                else: st.exception(Exception('Error en la carga de datos desde ByMA. Disculpe las molestias, estamos trabajando para solucionarlo.'))
-            with c2_2:    
-                if S.df_bonos_cor.all() is not None:
-                    st.subheader('Bonos Corporativos')
-                    t_1_cor,t_2_cor,t_3_cor=st.tabs(['Panel','Curva','Buscador'])
-                    with t_1_cor: st.dataframe(S.df_bonos_cor,use_container_width=True)
-                    with t_2_cor: st.subheader('Curva')
-                    with t_3_cor: 
-                        st.selectbox('Buscador de corpo',label_visibility='collapsed',options=S.df_bonos_cor.index.to_list(),key='corpobuscado')
-                        st.dataframe(S.df_bonos_cor.loc[S.df_bonos_cor.index==S.corpobuscado].transpose(),use_container_width=True)
-                else: st.exception(Exception('Error en la carga de datos desde ByMA. Disculpe las molestias, estamos trabajando para solucionarlo.'))
-            if S.df_iamc is not None:
-                c1_3,c2_3=st.columns((0.7,0.3))
-                with c1_3:
-                    st.subheader('Información de los bonos')
-                    st.dataframe(S.df_iamc)
-                with c2_3:
-                    st.subheader('Filtado de bono')
-                    st.selectbox('Buscador de Bonos',options=S.df_iamc.index.to_list(),key='bonobuscado')
-                    st.dataframe(S.df_iamc.loc[S.df_iamc.index==S.bonobuscado].transpose(),use_container_width=True)
-            else: st.exception(Exception('Error en la carga de datos desde ByMA. Disculpe las molestias, estamos trabajando para solucionarlo.'))
-            st.divider()
-            #TEST
-            try:
-                _=pd.merge(S.df_bonos_gob[['last','change','volume','expiration']],S.df_iamc[['Duración Modificada','Tir Anual']], left_index=True,right_index=True)
-                st.dataframe(_)
-            except:pass
+            make_bonds()
         with acciones:
             if (S.df_merval is not None):# and (S.df_general is not None):
                 fig_merv,fig_gen=make_acciones(S.df_merval,S.df_general)
