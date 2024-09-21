9b9e3fa1ac32699f9c7407a9bdc869242ab48709
FedericoBohl <federicobohl@uca.edu.ar>
2024-09-21 16:43:10 -0300

    test

--- Paginas/__SectorFinanciero__.py
+++ Paginas/__SectorFinanciero__.py
@@ -383,7 +383,7 @@ def make_acciones():
             </div>
             <!-- TradingView Widget END -->
         """
-        components.html(tradingview_widget, scrolling=False)
+        components.iframe(tradingview_widget, scrolling=False)
         tradingview_widget = """
         <!-- TradingView Widget BEGIN -->
         <div class="tradingview-widget-container">
@@ -481,7 +481,7 @@ def make_acciones():
         </div>
         <!-- TradingView Widget END -->
         """
-        components.html(tradingview_widget, scrolling=False)
+        components.iframe(tradingview_widget, scrolling=False)
     iframe_acciones()
 
 def make_bonds():
