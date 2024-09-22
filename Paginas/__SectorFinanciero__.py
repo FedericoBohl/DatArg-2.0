d6fe7f83e737e10d5dc6c957778ef6a1c7d9a2a8
FedericoBohl <federicobohl@uca.edu.ar>
2024-09-22 11:37:20 -0300

    test

--- Paginas/__SectorFinanciero__.py
+++ Paginas/__SectorFinanciero__.py
@@ -668,12 +668,13 @@ def make_cedears2():
         components.html(widget, height=200, scrolling=False)
         widget="""
                     <!-- TradingView Widget BEGIN -->
-                    <div class="tradingview-widget-container" style="height:100%;width:100%">
-                    <div class="tradingview-widget-container__widget" style="height:calc(100% - 32px);width:100%"></div>
+                    <div class="tradingview-widget-container">
+                    <div class="tradingview-widget-container__widget"></div>
                     <div class="tradingview-widget-copyright"><a href="https://es.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Siga los mercados en TradingView</span></a></div>
                     <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js" async>
                     {
-                    "autosize": true,
+                    "width": "100%",
+                    "height": "610",
                     "symbol": "NASDAQ:MSFT",
                     "timezone": "America/Argentina/Buenos_Aires",
                     "theme": "light",
