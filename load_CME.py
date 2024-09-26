7210295c623c93fb840c7b90b1c8dfcf20c1acd0
FedericoBohl <federicobohl@uca.edu.ar>
2024-09-26 14:50:06 -0300

    automatización código para cargar ponderaciones y donde se gasta

--- load_CME.py
+++ load_CME.py
@@ -1,113 +1,204 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import pandas as pd
-import time
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import os
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
-# Configuración del webdriver (asegúrate de tener el webdriver correcto instalado)
-
-
-def extract_data():
-    # Configurar opciones de Chrome para definir la carpeta de descargas
-    download_dir = os.getcwd()  # Esto usará la carpeta donde se ejecuta el script
-    chrome_options = webdriver.ChromeOptions()
-    prefs = {'download.default_directory': download_dir}
-    chrome_options.add_experimental_option('prefs', prefs)
-    # Inicializar el navegador
-    driver = webdriver.Chrome(options=chrome_options)
-    # Navega al sitio web
-    url = "https://cmegroup-tools.quikstrike.net/User/QuikStrikeView.aspx?viewitemid=IntegratedFedWatchTool&userId=lwolf&jobRole=&company=&companyType=&userId=lwolf&jobRole=&company=&companyType=&insid=134758201&qsid=1cb76c43-7e1c-453d-a81f-8305cb77294e"
-    url = "https://cmegroup-tools.quikstrike.net/User/QuikStrikeView.aspx?viewitemid=IntegratedFedWatchTool&userId=lwolf&jobRole=&company=&companyType=&userId=lwolf&jobRole=&company=&companyType=&insid=136996613&qsid=51e38ecf-9c47-4e24-ba42-5750dba40826"
+driver = webdriver.Chrome()
+# Navega al sitio web
+url='https://www.presupuestoabierto.gob.ar/sici/destacado-donde-se-gasta#'
+try:
     driver.get(url)
     time.sleep(5)
-    # Haz clic en la pestaña "Probabilities"
-    probabilities_tab = driver.find_element(By.ID, "ctl00_MainContent_ucViewControl_IntegratedFedWatchTool_lbPTree")
-    probabilities_tab.click()
-    time.sleep(5)
-    table = driver.find_elements(By.TAG_NAME, "table")[2]
-    rows = table.find_elements(By.TAG_NAME, "tr")
-    columns=[col.text for col in rows[1].find_elements(By.TAG_NAME, "th")]
-    data = []
-    for row in rows[2:]:
-        data.append(row.text.split(' '))
-    count=len(columns)
-    data2=[]
-    for i in range(len(data)):
-        if len(data[i])<count:
-            remplazar=[data[i][0]]
-            for j in range(count-len(data[i])):
-                remplazar.append('0.0%')
-            for j in data[i][1:]:
-                remplazar.append(j)
-            data2.append(remplazar)
-        else: data2.append(data[i])
-    df = pd.DataFrame(data, columns=columns)
-    df.to_csv('fed_rate_data.csv',index=False)
-
-    time.sleep(5)
-    # Haz clic en la pestaña "Probabilities"
-    dotplot_tab = driver.find_element(By.ID, "ctl00_MainContent_ucViewControl_IntegratedFedWatchTool_lbDotPlotTable")
-    dotplot_tab.click()
-    time.sleep(5)
-    table=driver.find_elements(By.TAG_NAME, "table")[1]
-    rows = table.find_elements(By.TAG_NAME, "tr")
-    column=rows[0].text.split(' ')
-    column.remove('RUN')
-    column[-1]='Largo Plazo'
-    column[0]=column[0].replace('\n',' ')
-    data=[]
-    time.sleep(1)
-    for row in rows[1:]:
-        row_data=[]
-        attempts = 0
-        success = False
-        while attempts<3:
-            try:
-                cols=row.find_elements(By.TAG_NAME, "td")
-                for i in cols:
-                    row_data.append(float(i.text) if i.text!='' else None)
-                success = True
-                break
-            except:
-                attempts += 1
-        data.append(row_data)
-    df=pd.DataFrame(data=data,columns=column)
-    df.to_csv('dotplot.csv',index=False)
-    
-    
-
-    # Datos del presupuesto
-    url = 'https://www.presupuestoabierto.gob.ar/sici/destacado-donde-se-gasta#'
-    driver.get(url)
-    time.sleep(5)
-    table_tab = driver.find_element(By.ID, "tab-tabla")
+    table_tab=WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "tab-tabla"))
+    )
+    #table_tab=driver.find_element(By.ID, "tab-tabla")
     table_tab.click()
-    download_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.CLASS_NAME, "export"))
+    # Esperar a que aparezca el span con la clase 'page-list'
+    page_list_span = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "page-list"))
     )
-    download_button.click()
 
-    csv_option = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//li[@data-type='csv']/a"))
+    # Dentro del span, encontrar el botón con la clase 'dropdown-toggle' y hacer clic
+    dropdown_button = page_list_span.find_element(By.CLASS_NAME, "dropdown-toggle")
+    dropdown_button.click()
+    time.sleep(1)
+    # Esperar a que el menú desplegable esté visible y seleccionar la opción '50'
+    #dropdown_menu = WebDriverWait(driver, 10).until(
+    #    EC.visibility_of_element_located((By.CLASS_NAME, "dropdown-menu"))
+    #)
+
+    # Encontrar la opción '50' dentro del menú desplegable y hacer clic en ella
+    option_50 = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, ".//li/a[text()='50']"))
     )
-    csv_option.click()
-    time.sleep(3)
+    option_50.click()
+    html = driver.page_source
+finally:
+    # Cerrar el driver
     driver.quit()
 
-extract_data()
-strikes=0
-while strikes!=3:
+# Parsear el contenido HTML con BeautifulSoup
+soup = BeautifulSoup(html, 'html.parser')
+
+
+# Buscar la tabla por su ID
+tabla = soup.find('table', {'id': 'tabla-programa'})
+
+# Inicializar una lista para almacenar los datos
+datos = []
+
+# Verificar si existe tbody, si no, buscar directamente en la tabla
+if tabla.find('tbody'):
+    filas = tabla.find('tbody').find_all('tr')
+else:
+    filas = tabla.find_all('tr')
+
+# Recorrer todas las filas
+for fila in filas:
+    # Extraer todas las celdas de la fila
+    celdas = fila.find_all('td')
+    # Extraer el texto de cada celda y almacenarlo en una lista
+    fila_datos = [celda.text.strip() for celda in celdas]
+    # Añadir la fila de datos a la lista principal
+    datos.append(fila_datos)
+
+# Crear un DataFrame con los datos extraídos
+columnas = ['Ejercicio', 'Ubicación Geográfica', 'Presupuestado', 'Ejecutado', '% Ejecutado']
+df = pd.DataFrame(datos, columns=columnas)
+df['Presupuestado']=[float(i.replace('.','').replace(',','.')) for i in df['Presupuestado']]
+df['Ejecutado']=[float(i.replace('.','').replace(',','.')) for i in df['Ejecutado']]
+df['% Ejecutado']=[float(i.replace('.','').replace(',','.')) for i in df['% Ejecutado']]
+# Mostrar el DataFrame
+df.to_csv("donde-se-gasta.csv")
+
+#############################################################################
+def load_iol(tipo_,url):
+    url=datasets[k]
+    response=requests.get(url)
+    soup=BeautifulSoup(response.text,'html.parser')
+    divs = soup.find_all("tr", {"data-cantidad": "1"})
+    ticker=[div.find('a').find("b").text.strip() for div in divs]
+    val=[float(div.find_all('td')[1].text.strip().replace('.','').replace(',','.')) for div in divs]
+    var=[float(div.find('span',{"data-field": "Variacion"}).text.replace('.','').replace(',','.')) for div in divs]
+    tipo=[tipo_ for div in divs]
+    return pd.DataFrame({'Ticker':ticker,'Precio':val,'Var':var,'Tipo':tipo})
+def get_ecovalores():
+    url='https://bonos.ecovalores.com.ar/eco/listado.php'
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+    soup=BeautifulSoup(response.text,'html.parser')
+    tables=soup.find_all('table')
+    data=None
+    for i in range(len(tables)):
+        try:
+            table=tables[i]
+            headers = table.find_all('th')
+            header_texts = [header.get_text(strip=True) for header in headers]
+            if 'TIR' in header_texts:
+                data=[[td.text for td in tr.find_all('td')] for tr in table.find_all('tr')]
+                data=pd.DataFrame(data,columns=header_texts).dropna()
+                data.set_index('Título',inplace=True)
+                data['TIR']=[float(i.replace('.','').replace('%','').replace(',','.')) for i in data['TIR']]
+                data['Duration']=[float(i.replace(',','.')) for i in data['Duration']]
+                data['Var %']=[float(i.replace('+','').replace(',','').replace('%','')) for i in data['Var %']]#[float(i.replace('.','').replace('%','').replace(',','.').replace('+','')) for i in data['Var %']]
+                data['Int. Corrido']=[float(i.replace('.','').replace(',','.')) for i in data['Int. Corrido']]
+                data['VT']=[float(i.replace('.','').replace(',','.')) for i in data['VT']]
+                data['Precio']=[float(i.replace('.','').replace(',','.')) for i in data['Precio']]
+                data['Vencimiento']=pd.to_datetime(data['Vencimiento'],format='%d/%m/%Y')
+                data['Próx. Vto.']=pd.to_datetime(data['Próx. Vto.'],format='%d/%m/%Y')
+                data.rename(columns={'VT':'Valor Técnico'},inplace=True)
+                break
+        except:continue
+    return data[['Nombre','Precio','Var %','Valor Técnico','Int. Corrido','TIR','Duration','Paridad','Vol %','Próx. Vto.','Vencimiento','Tipo']]
+
+datasets={
+        'Bonos':'https://iol.invertironline.com/mercado/cotizaciones/argentina/bonos/todos',
+        'Letras':'https://iol.invertironline.com/mercado/cotizaciones/argentina/letras/todas',
+        #'Obligaciones Negociables':'https://iol.invertironline.com/mercado/cotizaciones/argentina/obligaciones-negociables/todos'
+        }
+data=pd.DataFrame(columns=['Ticker','Precio','Var','Tipo'])
+for k in datasets:
+    data=pd.concat([data,load_iol(k,datasets[k])],ignore_index=True)
+data.set_index('Ticker',inplace=True)
+
+ecovalores=get_ecovalores()
+for i in ecovalores.index:
+    if i in data.index.values:
+        ecovalores.at[i,'Precio']=data.at[i,'Precio']
+        ecovalores.at[i,'Var %']=data.at[i,'Var']
+
+tipo2=[]
+for i in data.index:
     try:
-        extract_data()
-        print('Datos Obtenidos')
-        break
-    except Exception as e:
-        time.sleep(5)
-        strikes+=1
-        if strikes==3:
-            print(e)
-        pass
+        t=ecovalores.loc[i]['Tipo']
+    except:
+        if i=='BPOD7':
+            t='BOPREAL'
+        else:
+            t=None
+    tipo2.append(t)
+data['Tipo-2']=tipo2
+
+driver=webdriver.Chrome()
+
+nombre_=[]
+descripcion_=[]
+ley_=[]
+isin_=[]
+moneda_=[]
+amortizacion_=[]
+intereses_=[]
+residual_=[]
+
+for ticker in data.index:
+    try:
+        driver.get(f'https://www.cohen.com.ar/Bursatil/Especie/{ticker}/MERVAL/24hs.')
+        wait = WebDriverWait(driver, 10)
+        wait.until(EC.presence_of_element_located(("id", "tablaDatosPerfil")))
+        #time.sleep(5)
+        df=[[td.text for td in tr.find_elements(By.TAG_NAME,'td')] for tr in driver.find_element("id", "tablaDatosPerfil").find_elements(By.TAG_NAME,'tr')[1:]]
+        df=pd.DataFrame(df,columns=['Dato','Val']).set_index('Dato')
+        #print(df)
+        nombre=df.loc['Denominación']['Val'] if 'Denominación' in df.index else ''
+        descripcion=df.loc['Descripción']['Val'] if 'Descripción' in df.index else ''
+        ley=df.loc['Ley']['Val'] if 'Ley' in df.index else ''
+        isin=df.loc['Código ISIN']['Val'] if 'Código ISIN' in df.index else ''
+        moneda=df.loc['Moneda de Emisión']['Val'] if 'Moneda de Emisión' in df.index else ''
+        amortizacion=df.loc['Forma Amortización']['Val'] if 'Forma Amortización' in df.index else ''
+        intereses=df.loc['Interés']['Val'] if 'Interés' in df.index else ''
+        residual=float(df.loc['Monto Residual']['Val'].replace('.','').replace(',','.')) if 'Monto Residual' in df.index else 0
+        if residual ==0:
+            residual=float(df.loc['Monto de Emisión']['Val'].replace('.','').replace(',','.')) if 'Monto de Emisión' in df.index else 0
+    except:
+        nombre=''
+        descripcion= ''
+        ley=''
+        isin=''
+        moneda=''
+        amortizacion=''
+        intereses=''
+        residual=0
+    nombre_.append(nombre)
+    descripcion_.append(descripcion)
+    ley_.append(ley)
+    isin_.append(isin)
+    moneda_.append(moneda)
+    amortizacion_.append(amortizacion)
+    intereses_.append(intereses)
+    residual_.append(residual)
+    time.sleep(3.5)
+data['Nombre']=nombre_
+data['Descripción']=descripcion_
+data['Ley']=ley_
+data['ISIN']=isin_
+data['Moneda']=moneda_
+data['Amortización']=amortizacion_
+data['Intereses']=intereses_
+data['Monto Residual']=residual_
+data.to_csv("Datos Bonos.csv")
